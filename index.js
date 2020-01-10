7b9a66c43dbd66e1c2a0e64170270bf6610e5d95
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-10 14:28:46 -0500

    cors enable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //morgan is a middleware for allows log requests
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 //create server
@@ -8,6 +9,7 @@ app.set('PORT' , process.env.PORT || 5000);
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 //Routes
 app.use(require('./routes/index.js'));
